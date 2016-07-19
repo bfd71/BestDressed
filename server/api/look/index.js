5dc416f2dc4945f8bdfc9af2f014105968b95adb
bfd71 <brethen.crowne@gmail.com>
2016-07-19 04:50:31 -0400

    Modal Submit Button not working
    When attempting to upload a file from computer.  Modal submit is active
    when entering in title and description, but as soon as the file is
    selected the Submit button deactivates and no preview is visible.

--- server/api/look/index.js
+++ server/api/look/index.js
@@ -1,13 +1,13 @@
-"use strict";
+'use strict';
 
-var controller = require("./look.controller");
-var express = require("express");
+var controller = require('./look.controller');
+var express = require('express');
 var router = express.Router();
-var auth = require("../../auth/auth.service");
+var auth = require('../../auth/auth.service');
 
-// download image and save to db
-router.post("/scrapeUpload", auth.isAuthenticated(), controller.scrapeUpload);
-router.get("/getAllLooks", controller.allLooks);
+router.post('/scrapeUpload', auth.isAuthenticated(), controller.scrapeUpload);
+router.post('/upload', auth.isAuthenticated(), controller.upload);
 
+router.get('/getAllLooks', controller.allLooks);
 
 module.exports = router;
--- server/config/express.js
+++ server/config/express.js
@@ -9,6 +9,7 @@ var favicon = require('serve-favicon');
 var morgan = require('morgan');
 var compression = require('compression');
 var bodyParser = require('body-parser');
+var multer = require("multer");
 var methodOverride = require('method-override');
 var cookieParser = require('cookie-parser');
 var errorHandler = require('errorhandler');
@@ -20,45 +21,66 @@ var mongoStore = require('connect-mongo')(session);
 var mongoose = require('mongoose');
 
 module.exports = function(app) {
-  // var env = app.get('env');
-  var env = 'development';
+    // var env = app.get('env');
+    var env = 'development';
 
-  app.set('views', config.root + '/server/views');
-  app.engine('html', require('ejs').renderFile);
-  app.set('view engine', 'html');
-  app.use(compression());
-  app.use(bodyParser.urlencoded({
-    extended: false
-  }));
-  app.use(bodyParser.json());
-  app.use(methodOverride());
-  app.use(cookieParser());
-  app.use(passport.initialize());
+    app.set('views', config.root + '/server/views');
+    app.engine('html', require('ejs').renderFile);
+    app.set('view engine', 'html');
+    app.use(compression());
+    app.use(bodyParser.urlencoded({
+        extended: false
+    }));
+    app.use(bodyParser.json());
+    app.use(methodOverride());
 
-  // Persist sessions with mongoStore
-  // We need to enable sessions for passport twitter because its an oauth 1.0 strategy
-  app.use(session({
-    secret: config.secrets.session,
-    resave: true,
-    saveUninitialized: true,
-    store: new mongoStore({
-      mongooseConnection: mongoose.connection
-    })
-  }));
 
-  if ('production' === env) {
-    app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
-    app.use(express.static(path.join(config.root, 'public')));
-    app.set('appPath', config.root + '/public');
-    app.use(morgan('dev'));
-  }
+    // allows us to use request.middlewareStorage.fileimage as a reference to loacation
+    // of where our uploaded file exists
+    app.use(multer({
+        dest: '../client/assets/images/uploads/',
+        rename: function(fieldname, filename) {
+            return filename + Date.now();
+        },
+        onFileUploadStart: function(file) {
+            console.log(file.originalname + ' is starting...');
+        },
+        onFileUploadComplete: function(file, req, res) {
+            console.log(file.fieldname + ' uploaded to ' + file.path);
+            var fileimage = file.name;
+            req.middlewareStorage = {
+                fileimage: fileimage
+            }
+        }
+    }));
 
-  if ('development' === env || 'test' === env) {
-    // app.use(require('connect-livereload')());
-    app.use(express.static(path.join(config.root, '.tmp')));
-    app.use(express.static(path.join(config.root, 'client')));
-    app.set('appPath', 'client');
-    app.use(morgan('dev'));
-    app.use(errorHandler()); // Error handler - has to be last
-  }
+    app.use(cookieParser());
+    app.use(passport.initialize());
+
+    // Persist sessions with mongoStore
+    // We need to enable sessions for passport twitter because its an oauth 1.0 strategy
+    app.use(session({
+        secret: config.secrets.session,
+        resave: true,
+        saveUninitialized: true,
+        store: new mongoStore({
+            mongooseConnection: mongoose.connection
+        })
+    }));
+
+    if ('production' === env) {
+        app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
+        app.use(express.static(path.join(config.root, 'public')));
+        app.set('appPath', config.root + '/public');
+        app.use(morgan('dev'));
+    }
+
+    if ('development' === env || 'test' === env) {
+        // app.use(require('connect-livereload')());
+        app.use(express.static(path.join(config.root, '.tmp')));
+        app.use(express.static(path.join(config.root, 'client')));
+        app.set('appPath', 'client');
+        app.use(morgan('dev'));
+        app.use(errorHandler()); // Error handler - has to be last
+    }
 };
